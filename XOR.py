a16d4a609233a9b1b3bd888f8b0c2a50fabe642c
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2021-05-27 20:46:09 +0530

    Add files via upload

---
+++ XOR.py
@@ -0,0 +1,12 @@
+def XOR (a, b):
+    if a != b:
+        return True
+    else:
+        return False
+ 
+# Driver code
+if __name__=='__main__':
+    a = int(input("Enter a:"))
+    b = int(input("Enter b:"))
+    print(XOR(a, b))
+ 
