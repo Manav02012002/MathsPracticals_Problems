636940e1f6c85b2a36a5f5eae5e0fc50d28c0fd9
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2021-08-22 00:35:48 +0530

    Add files via upload

---
+++ Subgroup4.py
@@ -0,0 +1,20 @@
+def Operation(a,b,n):
+    return (a+b)%n
+
+def Subgroup(H, e, n):
+    for i in range(0,len(H)):
+        flag = False
+        for j in range(0,len(H)):
+            if Operation(H[i],H[j],n) not in H:
+                print(H[i],"*",H[j]," not in ", H)
+                print(H, "is not a subgroup")
+                return False
+            if Operation(H[i],H[j],n)==e:
+                flag = True
+        if flag == False:
+            print("No inverse for the element ", H[i])
+            print(H, "is not a subgroup")
+            return False
+    print(H, "is a subgroup")
+    return True
+Subgroup([0,4,6,8,10],0,12)
